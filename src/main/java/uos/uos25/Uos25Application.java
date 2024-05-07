670ca7e851f8ea4e8fe893738abade6b48e1a2b7
UDADDY <gustmd5715@gmail.com>
2024-05-07 13:06:47 +0900

    feat: init

---
+++ src/main/java/uos/uos25/Uos25Application.java
@@ -0,0 +1,13 @@
+package uos.uos25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uos25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uos25Application.class, args);
+	}
+
+}
---
+++ src/test/java/uos/uos25/Uos25ApplicationTests.java
@@ -0,0 +1,13 @@
+package uos.uos25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uos25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
