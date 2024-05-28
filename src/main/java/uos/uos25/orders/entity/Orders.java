26a015863e03eff613b700d3b70ec1a0d8b2a6b0
UDADDY <gustmd5715@gmail.com>
2024-05-28 13:44:38 +0900

    fix: 주문 Id Long으로 변경

--- src/main/java/uos/uos25/orders/entity/Orders.java
+++ src/main/java/uos/uos25/orders/entity/Orders.java
@@ -1,23 +1,28 @@
 package uos.uos25.orders.entity;
 
 import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import uos.uos25.product.entity.Product;
 import uos.uos25.shop.entity.Shop;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@NoArgsConstructor
 public class Orders {
     @Id
     private Integer ordersId;
 
     private LocalDateTime ordersDate;
     @Column(length = 20)
-    private String orderStatus;
+    private String ordersStatus;
     private Integer givenEa;
-    private Integer orderEa;
+    private Integer ordersEa;
     @Column(length = 18)
-    private String orderCheck;
+    private String ordersCheck;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "shop_id")
@@ -26,4 +31,22 @@ public class Orders {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id")
     private Product product;
+
+    @Builder
+    public Orders(String ordersStatus, Integer givenEa, Integer ordersEa, String ordersCheck, Shop shop, Product product){
+        this.ordersStatus = ordersStatus;
+        this.givenEa = givenEa;
+        this.ordersEa = ordersEa;
+        this.ordersCheck = ordersCheck;
+        this.shop = shop;
+        this.product = product;
+    }
+
+    public void setOrdersEa(Integer ea){
+        this.ordersEa = ea;
+    }
+
+    public void setOrdersStatus(String ordersStatus) {
+        this.ordersStatus = ordersStatus;
+    }
 }
