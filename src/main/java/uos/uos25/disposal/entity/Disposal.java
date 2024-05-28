60b70b1b812bd8695dae8744be867c188e3f7eb2
UDADDY <gustmd5715@gmail.com>
2024-05-28 13:57:26 +0900

    fix: Disposal Id Long으로 변경

--- src/main/java/uos/uos25/disposal/entity/Disposal.java
+++ src/main/java/uos/uos25/disposal/entity/Disposal.java
@@ -1,15 +1,20 @@
 package uos.uos25.disposal.entity;
 
 import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 import uos.uos25.product.entity.Product;
 import uos.uos25.shop.entity.Shop;
 
 import java.time.LocalDateTime;
 
 @Entity
+@NoArgsConstructor
 public class Disposal {
     @Id
-    private Integer disposalId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long disposalId;
+    private Integer ea;
     private LocalDateTime disposalDate;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -19,4 +24,11 @@ public class Disposal {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id")
     private Product product;
+
+    @Builder
+    public Disposal(Shop shop, Product product, Integer ea){
+        this.shop = shop;
+        this.product = product;
+        this.ea = ea;
+    }
 }
