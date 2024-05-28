32854bf01120759f566ef3be391970ed0ad88f34
UDADDY <gustmd5715@gmail.com>
2024-05-28 12:38:48 +0900

    feat: Product 조회 기능 추가

---
+++ src/main/java/uos/uos25/product/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package uos.uos25.product.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import uos.uos25.product.entity.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
