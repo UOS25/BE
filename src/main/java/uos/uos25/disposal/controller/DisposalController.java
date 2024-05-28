92824fbd67bf249d66f7bb028145a309018e180c
UDADDY <gustmd5715@gmail.com>
2024-05-28 14:55:03 +0900

    fix: 폐기 생성 DTO 이름 변경

--- src/main/java/uos/uos25/disposal/controller/DisposalController.java
+++ src/main/java/uos/uos25/disposal/controller/DisposalController.java
@@ -4,9 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import uos.uos25.disposal.dto.request.DisposalListRequestDTO;
+import uos.uos25.disposal.dto.request.DisposalReqeustDTO;
 import uos.uos25.disposal.entity.Disposal;
 import uos.uos25.disposal.service.DisposalService;
-import uos.uos25.returns.dto.request.ReturnsRequestDTO;
 
 import java.util.List;
 
@@ -17,8 +17,8 @@ public class DisposalController {
     private final DisposalService disposalService;
 
     @PostMapping
-    public ResponseEntity<Disposal> dispose(@RequestBody ReturnsRequestDTO returnsRequestDTO){
-        Disposal disposal = disposalService.createDisposal(returnsRequestDTO);
+    public ResponseEntity<Disposal> dispose(@RequestBody DisposalReqeustDTO disposalReqeustDTO){
+        Disposal disposal = disposalService.createDisposal(disposalReqeustDTO);
 
         return ResponseEntity.ok(disposal);
     }
--- src/main/java/uos/uos25/disposal/service/DisposalService.java
+++ src/main/java/uos/uos25/disposal/service/DisposalService.java
@@ -3,6 +3,7 @@ package uos.uos25.disposal.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import uos.uos25.disposal.dto.request.DisposalListRequestDTO;
+import uos.uos25.disposal.dto.request.DisposalReqeustDTO;
 import uos.uos25.disposal.entity.Disposal;
 import uos.uos25.disposal.repository.DisposalRepository;
 import uos.uos25.product.entity.Product;
@@ -21,14 +22,14 @@ public class DisposalService {
     private final ProductService productService;
     private final ShopService shopService;
 
-    public Disposal createDisposal(ReturnsRequestDTO returnsRequestDTO){
-        Shop shop = shopService.findShopById(returnsRequestDTO.getShopId());
-        Product product = productService.findProductById(returnsRequestDTO.getProductId());
+    public Disposal createDisposal(DisposalReqeustDTO disposalReqeustDTO){
+        Shop shop = shopService.findShopById(disposalReqeustDTO.getShopId());
+        Product product = productService.findProductById(disposalReqeustDTO.getProductId());
 
         Disposal disposal = Disposal.builder()
                 .shop(shop)
                 .product(product)
-                .ea(returnsRequestDTO.getEa())
+                .ea(disposalReqeustDTO.getEa())
                 .build();
 
         // TODO: 폐기 개수 지워야 하나?? 인벤토리에서 삭제?
