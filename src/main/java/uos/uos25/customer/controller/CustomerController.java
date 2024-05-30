99ecaae5fb6459ab7d38c115ba86a1b902009a51
mtsaurus <qksrurwnsql@naver.com>
2024-05-31 00:00:48 +0900

    feat: Customer 주 기능 구현

---
+++ src/main/java/uos/uos25/customer/controller/CustomerController.java
@@ -0,0 +1,68 @@
+package uos.uos25.customer.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uos.uos25.customer.DTO.request.CustomerRequestDTO;
+import uos.uos25.customer.DTO.response.CustomerResponseDTO;
+import uos.uos25.customer.service.CustomerService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/customer")
+public class CustomerController {
+    private final CustomerService customerService;
+
+    // create
+    @PostMapping("/join")
+    public ResponseEntity<?> joinCustomer(@Valid @RequestBody CustomerRequestDTO customerRequestDTO) {
+        customerService.createCustomer(customerRequestDTO);
+
+        String msg = "고객 등록이 완료되었습니다.";
+
+        return new ResponseEntity<>(msg, HttpStatus.OK);
+    }
+
+    // readAll
+    @GetMapping
+    public ResponseEntity<List<CustomerResponseDTO>> findAllCustomers() {
+        return ResponseEntity.ok(customerService.findAllCustomers());
+    }
+
+    // readById
+    @GetMapping("/{customerId}")
+    public ResponseEntity<CustomerResponseDTO> findCustomerById(@PathVariable Long customerId) {
+        return ResponseEntity.ok(customerService.findCustomerById(customerId));
+    }
+
+    // update
+    @PutMapping("/update/{customerId}")
+    public ResponseEntity<?> updateCustomerById(@PathVariable Long customerId, @RequestBody CustomerRequestDTO customerRequestDTO) {
+        customerService.updateCustomer(customerRequestDTO, customerId);
+
+        String msg = "고객 수정이 완료되었습니다.";
+        return new ResponseEntity<>(msg, HttpStatus.OK);
+    }
+
+    // 마일리지 적립
+    @PostMapping("/mileage/{customerId}")
+    public ResponseEntity<?> earnMileage(@PathVariable Long customerId, @RequestBody Integer mileage) {
+        customerService.earnMileage(mileage, customerId);
+
+        String msg = "마일리지 적립이 완료되었습니다.";
+        return new ResponseEntity<>(msg, HttpStatus.OK);
+    }
+
+    // delete
+    @DeleteMapping("/delete/{customerId}")
+    public ResponseEntity<?> deleteCustomer(@PathVariable Long customerId) {
+        customerService.deleteCustomer(customerId);
+
+        String msg = "고객 삭제가 완료되었습니다.";
+        return new ResponseEntity<>(msg, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/uos/uos25/customer/service/CustomerService.java
@@ -0,0 +1,71 @@
+package uos.uos25.customer.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import uos.uos25.customer.DTO.request.CustomerRequestDTO;
+import uos.uos25.customer.DTO.response.CustomerResponseDTO;
+import uos.uos25.customer.entity.Customer;
+import uos.uos25.customer.exception.CustomerNotFoundException;
+import uos.uos25.customer.repository.CustomerRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class CustomerService {
+    private final CustomerRepository customerRepository;
+
+    // create
+    // 고객 정보를 생성합니다.
+    public void createCustomer(CustomerRequestDTO customerRequestDTO) {
+        Customer customer = Customer.builder()
+                .customerHP(customerRequestDTO.getCustomerHP())
+                .nickname(customerRequestDTO.getNickname())
+                .build();
+
+        customerRepository.save(customer);
+    }
+
+    // read
+    // 조회 가능한 모든 고객 정보를 불러옵니다.
+    public List<CustomerResponseDTO> findAllCustomers() {
+        List<Customer> allCustomers =  customerRepository.findAll();
+
+        return allCustomers.stream().map(CustomerResponseDTO::fromEntity).toList();
+    }
+
+    // read
+    // 고객 아이디로 해당 고객의 정보를 불러옵니다.
+    public CustomerResponseDTO findCustomerById(Long customerId) {
+        Customer findCustomer = customerRepository.findById(customerId)
+                .orElseThrow(() -> new CustomerNotFoundException("해당 아이디의 고객이 존재하지 않습니다."));
+        return CustomerResponseDTO.fromEntity(findCustomer);
+    }
+
+    // update
+    // 고객 정보를 업데이트합니다.
+    public void updateCustomer(CustomerRequestDTO customerRequestDTO, Long customerId) {
+        Customer findCustomer = customerRepository.findById(customerId)
+                .orElseThrow(() -> new CustomerNotFoundException("해당 아이디의 고객이 존재하지 않습니다."));
+        // 정보 수정
+        findCustomer.changeCustomerInfo(customerRequestDTO.getCustomerHP(), customerRequestDTO.getNickname());
+    }
+
+    // update
+    // 마일리지를 적립합니다. 적립되는 마일리지 양은 이벤트 등을 고려하여, 판매 과정에서 계산됩니다.
+    public void earnMileage(Integer mileage, Long customerId) {
+        Customer findCustomer = customerRepository.findById(customerId)
+                .orElseThrow(() -> new CustomerNotFoundException("해당 아이디의 고객이 존재하지 않습니다."));
+
+        // 마일리지를 적립합니다.
+        findCustomer.earnMileage(mileage);
+    }
+
+    // delete
+    // 고객 정보를 삭제합니다.
+    public void deleteCustomer(Long customerId) {
+        customerRepository.deleteById(customerId);
+    }
+}
