4a9df8f7ce0008638742248993637040322e8873
mtsaurus <qksrurwnsql@naver.com>
2024-05-31 01:48:43 +0900

    feat: DataInitializer 더미 데이터 추가

---
+++ src/main/java/uos/uos25/headQuarter/repository/HeadQuarterRepository.java
@@ -0,0 +1,7 @@
+package uos.uos25.headQuarter.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import uos.uos25.headQuarter.entity.HeadQuarter;
+
+public interface HeadQuarterRepository extends JpaRepository<HeadQuarter, Long> {
+}
