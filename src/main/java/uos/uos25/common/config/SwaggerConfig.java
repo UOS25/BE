82c43ff1f4e2e3ba6ed5eedc59926bfebaa5a6da
UDADDY <gustmd5715@gmail.com>
2024-06-05 13:55:53 +0900

    feat: 스웨거 추가

---
+++ src/main/java/uos/uos25/common/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package uos.uos25.common.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo(){
+        return new Info()
+                .title("UOS25 API")
+                .description("서울시립대학교 UOS25 API")
+                .version("1.0.0");
+    }
+}
