9dcb43efc2eb14629657637929d200b1d47da828
UDADDY <gustmd5715@gmail.com>
2024-06-05 19:26:32 +0900

    refator: request dto 예시 추가

--- src/main/java/uos/uos25/customer/dto/request/CustomerMileageUpdateRequestDTO.java
+++
@@ -1,9 +0,0 @@
-package uos.uos25.customer.dto.request;
-
-import lombok.Data;
-
-@Data
-public class CustomerMileageUpdateRequestDTO {
-    private final String phoneNumber;
-    private final Integer mileage;
-}
