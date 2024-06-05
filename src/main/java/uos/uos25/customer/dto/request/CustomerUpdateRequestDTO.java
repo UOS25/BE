5740faa200b2c22ca74a77469438d0d9bc8e7dab
UDADDY <gustmd5715@gmail.com>
2024-06-05 19:33:22 +0900

    chore: request dto 예시 추가

--- src/main/java/uos/uos25/customer/dto/request/CustomerUpdateRequestDTO.java
+++ src/main/java/uos/uos25/customer/dto/request/CustomerUpdateRequestDTO.java
@@ -2,6 +2,7 @@ package uos.uos25.customer.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
@@ -12,8 +13,10 @@ import lombok.Getter;
 public class CustomerUpdateRequestDTO {
 
     @NotBlank(message = "전화번호를 입력하세요.")
+    @Schema(example = "01012341234")
     private final String phoneNumber;
 
     @NotBlank(message = "이름을 입력하세요.")
+    @Schema(example = "유현승")
     private final String nickname;
 }
