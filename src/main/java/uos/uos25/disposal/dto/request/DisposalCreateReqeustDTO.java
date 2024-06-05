5b2dce72ea860db1d8fd9e9ec374aa34aa80fe92
UDADDY <gustmd5715@gmail.com>
2024-06-06 01:44:19 +0900

    refactor: 폐기 조회 기능 리팩토링

---
+++ src/main/java/uos/uos25/disposal/dto/request/DisposalCreateReqeustDTO.java
@@ -0,0 +1,16 @@
+package uos.uos25.disposal.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+@Data
+public class DisposalCreateReqeustDTO {
+    @Schema(example = "1")
+    private final Long shopId;
+
+    @Schema(example = "barcode")
+    private final String barcode;
+
+    @Schema(example = "3")
+    private final Integer ea;
+}
