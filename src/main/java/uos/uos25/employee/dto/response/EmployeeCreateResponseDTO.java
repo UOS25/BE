0046e984f3483177331d24d52b32a6f617cee390
UDADDY <gustmd5715@gmail.com>
2024-06-05 19:12:04 +0900

    fix: 직원 생성 시 응답값에 id 추가

--- src/main/java/uos/uos25/employee/dto/response/EmployeeCreateResponseDTO.java
+++ src/main/java/uos/uos25/employee/dto/response/EmployeeCreateResponseDTO.java
@@ -1,38 +1,23 @@
 package uos.uos25.employee.dto.response;
 
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-
 import lombok.Builder;
 import lombok.Data;
 import uos.uos25.employee.entity.Employee;
 
 @Data
 public class EmployeeCreateResponseDTO {
-
-    @NotBlank(message = "직원 이름을 입력하세요.")
+    private Long employeeId;
     private String employeeName;
-
-    @NotBlank(message = "직급을 입력하세요.")
     private String position;
-
-    @NotBlank(message = "주민번호를 입력하세요.")
     private String registrationNumber;
-
-    @Min(value = -1, message = "0 이상의 값을 입력하세요.")
     private Integer salary;
-
-    @NotBlank(message = "업무 시간을 입력하세요.")
     private String partTime;
-
-    @NotBlank(message = "계좌번호를 입력하세요.")
     private String account;
-
-    @NotBlank(message = "지점이름을 입력하세요.")
     private Long shopId;
 
     @Builder
     public EmployeeCreateResponseDTO(
+            Long employeeId,
             String employeeName,
             String position,
             String registrationNumber,
@@ -40,6 +25,7 @@ public class EmployeeCreateResponseDTO {
             String partTime,
             String account,
             Long shopId) {
+        this.employeeId = employeeId;
         this.employeeName = employeeName;
         this.position = position;
         this.registrationNumber = registrationNumber;
@@ -51,6 +37,7 @@ public class EmployeeCreateResponseDTO {
 
     public static EmployeeCreateResponseDTO fromEntity(Employee employee) {
         return EmployeeCreateResponseDTO.builder()
+                .employeeId(employee.getEmployeeId())
                 .employeeName(employee.getEmployeeName())
                 .position(employee.getPosition())
                 .registrationNumber(employee.getRegistrationNumber())
