6644157319f2122e925fcb0f45ddfe6729b27eb6
UDADDY <gustmd5715@gmail.com>
2024-06-06 22:41:33 +0900

    feat: 에러 코드 작성

---
+++ src/main/java/uos/uos25/common/error/ErrorCode.java
@@ -0,0 +1,57 @@
+package uos.uos25.common.error;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum ErrorCode {
+    // Common
+    INTERNAL_SERVER_ERROR(
+            HttpStatus.INTERNAL_SERVER_ERROR.value(), "Internal Server Error Occured."),
+
+    // Customer
+    CUSTOMER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "Customer is not found."),
+
+    // Employee
+    EMPLOYEE_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "Employee is not found."),
+
+    // Employee WorkingHistory
+    DUPLICATED_HISTORY(HttpStatus.BAD_REQUEST.value(), "Working History is duplicated."),
+
+    // Event
+    EVENT_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "Event is not found."),
+
+    // Inventory
+    INVENTORY_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "Inventory is not found."),
+    INVENTORY_EA_NOT_ENOUGH(HttpStatus.BAD_REQUEST.value(), "Inventory ea is not enough."),
+    INVENTORY_TOO_MANY_DISPLAY(
+            HttpStatus.BAD_REQUEST.value(), "Display ea can't be greater than inventory ea."),
+    TOO_MANY_PRODUCT_PURCHASE(
+            HttpStatus.BAD_REQUEST.value(), "Purchase ea can't be greater than display ea."),
+
+    // Orders
+    ORDERS_NOT_FOUND(HttpStatus.NO_CONTENT.value(), "Orders is not found."),
+
+    // Product
+    PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "Prodcut is not found."),
+    TOO_MANY_MILEAGE(
+            HttpStatus.BAD_REQUEST.value(), "Mileage can't be greater than purchase price."),
+
+    // Receipt
+    RECEIPT_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "Receipt is not found."),
+
+    // ReceiptDetails
+    RECEIPT_DETAILS_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "Receipt Details is not found."),
+
+    // Shop
+    SHOP_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "Shop is not found."),
+
+    // Disbursement
+    DISBURSEMENT_ALREADY_EXISTS(HttpStatus.BAD_REQUEST.value(), "Disbursement already exists.");
+
+    private Integer status;
+    private String message;
+}
