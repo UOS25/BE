afdba7e0e49cad3861fff5c24cb65e29ac06cc88
UDADDY <gustmd5715@gmail.com>
2024-06-06 22:49:46 +0900

    refactor: 결제 예외 처리 리팩토링

--- src/main/java/uos/uos25/purchase/exception/TooManyMileageException.java
+++ src/main/java/uos/uos25/purchase/exception/TooManyMileageException.java
@@ -1,3 +1,10 @@
 package uos.uos25.purchase.exception;
 
-public class TooManyMileageException extends RuntimeException {}
+import uos.uos25.common.error.BusinessException;
+import uos.uos25.common.error.ErrorCode;
+
+public class TooManyMileageException extends BusinessException {
+    public TooManyMileageException() {
+        super(ErrorCode.TOO_MANY_MILEAGE);
+    }
+}
