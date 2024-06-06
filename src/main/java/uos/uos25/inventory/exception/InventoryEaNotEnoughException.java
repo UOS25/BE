cd25a8e65d83cb1a6165179be485a5861d3eb327
UDADDY <gustmd5715@gmail.com>
2024-06-06 22:46:28 +0900

    refactor: 재고 예외 처리 리팩토링

--- src/main/java/uos/uos25/inventory/exception/InventoryEaNotEnoughException.java
+++ src/main/java/uos/uos25/inventory/exception/InventoryEaNotEnoughException.java
@@ -1,3 +1,10 @@
 package uos.uos25.inventory.exception;
 
-public class InventoryEaNotEnoughException extends RuntimeException {}
+import uos.uos25.common.error.BusinessException;
+import uos.uos25.common.error.ErrorCode;
+
+public class InventoryEaNotEnoughException extends BusinessException {
+    public InventoryEaNotEnoughException() {
+        super(ErrorCode.INVENTORY_EA_NOT_ENOUGH);
+    }
+}
