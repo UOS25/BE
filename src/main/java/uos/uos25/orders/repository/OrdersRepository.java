7ed486f12d74c7b2bec050ce41d04113a1c4992c
UDADDY <gustmd5715@gmail.com>
2024-06-09 00:30:44 +0900

    feat: 지점으로 주문 조회 추가

--- src/main/java/uos/uos25/orders/repository/OrdersRepository.java
+++ src/main/java/uos/uos25/orders/repository/OrdersRepository.java
@@ -1,9 +1,13 @@
 package uos.uos25.orders.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import uos.uos25.orders.entity.Orders;
 
 @Repository
-public interface OrdersRepository extends JpaRepository<Orders, Long> {}
+public interface OrdersRepository extends JpaRepository<Orders, Long> {
+    List<Orders> findAllByShopShopId(Long shopId);
+}
