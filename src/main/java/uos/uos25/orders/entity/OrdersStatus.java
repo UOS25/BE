d207633d05a867768ba1341836b3c7d123cb5dc0
UDADDY <gustmd5715@gmail.com>
2024-06-09 09:09:41 +0900

    fix: 주문 예외 추가

--- src/main/java/uos/uos25/orders/entity/OrdersStatus.java
+++ src/main/java/uos/uos25/orders/entity/OrdersStatus.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 public enum OrdersStatus {
     REQUEST("주문요청"),
     DELIVERED("배송중"),
-    STORED("입고");
+    STORED("입고"),
+    REFUNDED("반품");
 
     private String status;
 }
