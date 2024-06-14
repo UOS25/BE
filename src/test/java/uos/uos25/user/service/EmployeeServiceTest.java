31c859ddaf9e448bc6c9274b33cc6b4904f664b4
UDADDY <gustmd5715@gmail.com>
2024-06-14 16:13:05 +0900

    test: 직원 생성 테스트 코드 작성

--- src/test/java/uos/uos25/user/service/EmployeeServiceTest.java
+++ src/test/java/uos/uos25/user/service/EmployeeServiceTest.java
@@ -7,9 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import uos.uos25.shop.repository.ShopRepository;
+import uos.uos25.user.dto.request.EmployeeCreateReqeustDTO;
 import uos.uos25.user.entity.Employee;
-import uos.uos25.user.repository.EmployeeRepository;
+import uos.uos25.user.entity.PartTime;
 
 @SpringBootTest
 @Transactional
@@ -23,4 +23,28 @@ class EmployeeServiceTest {
         assertThat(employee.getEmployeeName()).isEqualTo("박직원");
     }
 
+    @Test
+    void 직원_생성() {
+        EmployeeCreateReqeustDTO employeeCreateReqeustDTO =
+                new EmployeeCreateReqeustDTO(
+                        "유현승",
+                        "점원",
+                        "990222-1056789",
+                        12000,
+                        PartTime.DAY.name(),
+                        "110-485-577342",
+                        "신한은행",
+                        1L);
+        Employee savedEmployee = employeeService.saveEmployee(employeeCreateReqeustDTO);
+
+        assertThat(savedEmployee.getEmployeeId()).isNotNull();
+        assertThat(savedEmployee.getEmployeeName()).isEqualTo("유현승");
+        assertThat(savedEmployee.getPosition()).isEqualTo("점원");
+        assertThat(savedEmployee.getRegistrationNumber()).isEqualTo("990222-1056789");
+        assertThat(savedEmployee.getPartTime()).isEqualTo(PartTime.DAY);
+        assertThat(savedEmployee.getAccount()).isEqualTo("110-485-577342");
+        assertThat(savedEmployee.getBank()).isEqualTo("신한은행");
+        assertThat(savedEmployee.getShop()).isNotNull();
+        assertThat(savedEmployee.getFiredDate()).isNull();
+    }
 }
